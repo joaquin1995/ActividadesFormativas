8752c5c3c9c6ac5e0900c5157b72896d0edaa328
joaquin <joakincortez8@gmail.com>
2019-10-01 17:10:13 -0400

    joaquin

---
+++ js/init.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+
+
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+
+
+    var options = {
+
+        data: {
+            "SISTEMAS": null,
+            "REDES": null,
+            "FINACIERA": 'https://placehold.it/250x250'
+        },
+
+        onAutocomplete: function(text){
+            console.log('si',text);
+            
+        }
+    }
+
+
+    var elems = document.querySelectorAll('.autocomplete');
+    var instances = M.Autocomplete.init(elems, options);
+
+
+    var options1 = {
+
+        data: {
+            "PROGRAMACION 1": null,
+            "COMERCIO ELECTRONICO": null,
+            "BASES DE DATOS": 'https://placehold.it/250x250'
+        },
+
+        onAutocomplete: function(text){
+            console.log('si',text);
+            
+        }
+    }
+
+
+    var elems = document.querySelectorAll('.autocomplete1');
+    var instances = M.Autocomplete.init(elems, options1);
+  });
+
